3ae4a63ec7c983e8a671bb5f4b2549c8129b569f
Alan Falloon <alan.falloon@gmail.com>
2010-09-13 20:30:14 -0400

    Normalize sentence structure

--- Main.hs
+++ Main.hs
@@ -6,6 +6,7 @@ import qualified Data.Map as M
 import qualified Data.ByteString.Lazy.Char8 as B
 
 dot = B.pack "."
+comma = B.pack ","
 
 sentenceEnds :: S.Set B.ByteString
 sentenceEnds = S.fromList [ dot
@@ -13,6 +14,13 @@ sentenceEnds = S.fromList [ dot
                           , B.pack "?"
                           , B.pack ":"
                           ]
+
+clauseEnds :: S.Set B.ByteString
+clauseEnds = S.fromList [ comma
+                        , B.pack ";"
+                        , B.pack "("
+                        , B.pack ")"
+                        ]
 
 isEnd :: B.ByteString -> Bool
 isEnd b = b `S.member` sentenceEnds
@@ -24,6 +32,18 @@ sentences w = words : sentences rest'
       (words, rest) = break isEnd w
       rest' = dropWhile isEnd rest
 
+-- | Convert the sentences to a normal form suitable for trigram
+-- extraction. The sentence will start with 2 dots and end with a
+-- single dot. This ensures that the start and end conditions for the
+-- sentences are known. Clauses (such as parenthetical asides, or
+-- dependant clauses) are separated using 2 commas so that their state
+-- is separated.
+normalizeSentence :: [B.ByteString] -> [B.ByteString]
+normalizeSentence s = [dot,dot] ++ concatMap normalizeClauses s ++ [dot]
+    where
+      normalizeClauses w | w `S.member` clauseEnds = [comma,comma]
+                         | otherwise               = [w]
+
 main = do
   contents <- B.getContents
-  mapM_ (print . map B.unpack) (sentences $ wordSplit contents)
+  mapM_ (print . map B.unpack) (map normalizeSentence $ sentences $ wordSplit contents)
